74040727f81137d19552fe51b3d7a6a6321af0cc
Ritik Sachdev <ritiksachdeva50@iitkgp.ac.in>
2023-08-27 02:42:24 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open('requirements.txt') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
 setup(
     name = 'mlproject',
@@ -6,5 +19,5 @@ setup(
     author = 'ritik',
     author_email = 'ritiksachdeva50@iitkgp.ac.in',
     packages = find_packages(),
-    install_requires = ['pandas', 'numpy', ]
+    install_requires = get_requirements('requirements.txt')
 )
